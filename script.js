9993d65052f5c5c014acfa1fcc7fa0682231658b
Pablo <gomezmateo0000@gmail.com>
2025-02-13 16:48:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.btn');
+    
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            alert('Esta funcionalidad aún no está implementada. ¡Próximamente!');
+        });
+    });
+});
